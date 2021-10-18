69157c8d30d75fca03bca7d6f4a2101cad58366d
Romulo Ciro <romulo.ciro@hotmail.com>
2021-10-18 17:34:37 -0300

    styles: add styles dashboard

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.div`
+
+  .cardGrid {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .card {
+    width: 60%;
+  }
+
+  .ant-card-head {
+    text-align: center;
+  }
+
+  .ant-card-body{
+    text-align: center;
+  }
+`;
