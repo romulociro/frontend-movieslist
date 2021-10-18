39412c9c9a0e2ce415f4981071b356921b6051f4
Romulo Ciro <romulo.ciro@hotmail.com>
2021-10-18 11:08:26 -0300

    feat: add authcontext

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,12 +1,12 @@
-import React, { useCallback, useContext, useRef } from 'react';
+import React, { useCallback, useContext } from 'react';
 import { FiLogIn, FiUser, FiLock } from 'react-icons/fi';
 import * as Yup from 'yup';
 import { Form } from '@unform/web';
-import { FormHandles } from '@unform/core';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logoteste.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import { AuthContext } from '../../context/AuthContext';
 
 interface ISignFormProps{
   username: string;
@@ -14,6 +14,8 @@ interface ISignFormProps{
 }
 
 const SignIn: React.FC = () => {
+  const { signIn } = useContext(AuthContext);
+
   const handleSubmit = useCallback(async (data: ISignFormProps) => {
     try {
       const schema = Yup.object().shape({
@@ -24,10 +26,14 @@ const SignIn: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false,
       });
+      signIn({
+        username: data.username,
+        password: data.password,
+      });
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [signIn]);
 
   return (
     <Container>
