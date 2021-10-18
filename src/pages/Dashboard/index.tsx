d08b6721d35b909f130b6812c2a27e80ae7a3c66
Romulo Ciro <romulo.ciro@hotmail.com>
2021-10-18 17:34:16 -0300

    feat: add page dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,67 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Card } from 'antd';
+import { Link } from 'react-router-dom';
+import { CardContainer } from './styles';
+import { Header } from '../../components/Header';
+import 'antd/dist/antd.css';
+import api from '../../services/api';
+
+const Dashboard: React.FC = () => {
+  const token = localStorage.getItem('@MovieList:token');
+  const [movies, setMovies] = useState<any[]>([]);
+
+  const userData = async () => {
+    try {
+      const res: any = await api.get('http://localhost:3333/movies', {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      });
+      setMovies(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    userData();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <CardContainer>
+        <Card title="Movies">
+          {movies.map((movie) => (
+            <Card.Grid className="cardGrid">
+              <Card
+                className="card"
+                size="small"
+                title={movie.title}
+                extra={<Link to="/">More</Link>}
+              >
+                <p>
+                  Diretor:
+                  {' '}
+                  {movie.director}
+                </p>
+                <p>
+                  Categoria:
+                  {' '}
+                  {movie.category}
+                </p>
+                <p>
+                  Ano:
+                  {' '}
+                  {movie.year}
+                </p>
+              </Card>
+            </Card.Grid>
+          ))}
+        </Card>
+      </CardContainer>
+    </>
+  );
+};
+
+export default Dashboard;
